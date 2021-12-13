8d8df3524133db037450b64ba3a6d2e06242ad6e
Jenei Péter <jeneipeter001@gmail.com>
2021-12-13 22:04:28 +0100

    2. feladat kész

--- FeladatNyilvantarto/MainWindow.xaml.cs
+++ FeladatNyilvantarto/MainWindow.xaml.cs
@@ -21,9 +21,11 @@ namespace FeladatNyilvantarto
     /// </summary>
     public partial class MainWindow : Window
     {
-        //List<string> feladatokStringList;
         List<CheckBox> feladatokLista = new List<CheckBox>();
         List<CheckBox> toroltFelLista = new List<CheckBox>();
+
+        string kijeloltFeladat = "";
+        string kijeloltToroltFel = "";
 
         public MainWindow()
         {
@@ -137,14 +139,12 @@ namespace FeladatNyilvantarto
             sorokList.RemoveAll(s => s == "");
             sorokList[sor] = ujSzoveg;
             File.WriteAllLines(fajlNev, sorokList);
-
-
         }
 
         private void feladatTorles_Click(object sender, RoutedEventArgs e)
         {
-            if (feladatokListaLBx.SelectedItem == null)
-                return;
+            //if (feladatokListaLBx.SelectedItem == null)
+            //    return;
 
             string felNev = "";
             int isChecked = 0;
@@ -163,9 +163,88 @@ namespace FeladatNyilvantarto
             toroltFelLista.RemoveAll(s => s == "");
             File.WriteAllLines("toroltFeladatok.dat", toroltFelLista);
 
-            CbListaFeltoltes(this.feladatokLista, "feladatok.dat");     // Feladatok lista elemeinek frissítése
-            CbListaFeltoltes(this.toroltFelLista, "feladatok.dat");     // Törölt feladatok lista elemeinek frissítése
-            ListBoxokFeltoltese();                                      // Feladatok ListBox elemeinek frissítése
+            CbListaFeltoltes(this.feladatokLista, "feladatok.dat");             // Feladatok lista elemeinek frissítése
+            CbListaFeltoltes(this.toroltFelLista, "toroltFeladatok.dat");       // Törölt feladatok lista elemeinek frissítése
+            ListBoxokFeltoltese();                                              // Feladatok ListBox elemeinek frissítése
+        }
+
+        private void feladatokListaLBx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (feladatokListaLBx.SelectedItem != null)
+            {
+                string kijeloltELem = feladatokListaLBx.SelectedItem.ToString();
+                kijeloltELem = kijeloltELem.Remove(0, 41);
+                kijeloltELem = kijeloltELem.Remove(kijeloltELem.IndexOf(" IsChecked:"));
+
+                feladatSzovegInput.Text = kijeloltELem;
+                kijeloltFeladat = kijeloltELem;
+            }
+        }
+
+        private void feladatModositas_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> felList = File.ReadAllLines("feladatok.dat").ToList();
+
+            for (int f = 0; f < felList.Count; f++)
+            {
+                if (felList[f].StartsWith(kijeloltFeladat + "|"))
+                {
+                    felList[f] = feladatSzovegInput.Text + "|" + felList[f].Substring(kijeloltFeladat.Length + 1);
+                }
+            }
+
+            File.WriteAllLines("feladatok.dat", felList);
+
+            CbListaFeltoltes(feladatokLista, "feladatok.dat");
+            ListBoxokFeltoltese();
+        }
+
+        private void toroltElemekLBx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (toroltElemekLBx.SelectedItem != null)
+            {
+                string kijeloltELem = toroltElemekLBx.SelectedItem.ToString();
+                kijeloltELem = kijeloltELem.Remove(0, 41);
+                kijeloltELem = kijeloltELem.Remove(kijeloltELem.IndexOf(" IsChecked:"));
+
+                kijeloltToroltFel = kijeloltELem;
+            }
+        }
+
+        private void kijeloltFelVisszaBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //if (toroltElemekLBx.SelectedItem == null)
+            //    return;
+
+            string felNev = "";
+            int isChecked = 0;
+
+            string kijeloltELem = toroltElemekLBx.SelectedItem.ToString();
+            felNev = kijeloltELem.Remove(0, 41);
+            felNev = felNev.Remove(felNev.IndexOf(" IsChecked:"));
+            isChecked = kijeloltELem.EndsWith("True") ? 1 : 0;
+
+            List<string> toroltFelLista = File.ReadAllLines("toroltFeladatok.dat").ToList();
+            toroltFelLista.RemoveAll(s => s.StartsWith(felNev + "|"));
+            File.WriteAllLines("toroltFeladatok.dat", toroltFelLista);
+
+            var sorok = File.ReadAllLines("feladatok.dat").ToList();
+            sorok.Add($"{felNev}|{isChecked}");
+            sorok.RemoveAll(s => s == "");
+            File.WriteAllLines("feladatok.dat", sorok);
+
+            CbListaFeltoltes(this.feladatokLista, "feladatok.dat");             // Feladatok lista elemeinek frissítése
+            CbListaFeltoltes(this.toroltFelLista, "toroltFeladatok.dat");       // Törölt feladatok lista elemeinek frissítése
+            ListBoxokFeltoltese();
+        }
+
+        private void kijeloltFelVeglegTorleseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> toroltFelLista = File.ReadAllLines("toroltFeladatok.dat").ToList();
+            toroltFelLista.RemoveAll(f => f.StartsWith(kijeloltToroltFel + "|"));
+            File.WriteAllLines("toroltFeladatok.dat", toroltFelLista);
+
+            CbListaFeltoltes(this.toroltFelLista, "toroltFeladatok.dat"); 
         }
     }
 }
